0ce8f07ed86bb5956cec90ca92b88208b599cef7
Clay Smith <uhclay@gmail.com>
2015-05-24 22:54:05 -0500

    Breakout roll module

--- bot/SurgeBot.js
+++ bot/SurgeBot.js
@@ -5,17 +5,18 @@ var irc = require('irc'),
     Docs = require('./Docs.js'),
     Q = require('q'),
     nodemailer = require('nodemailer'),
-    bcrypt = require('bcrypt-nodejs'),
     db = require('./Database')
     auth = require('./Auth');
+    roll = require('./Roll');
 
 module.exports = SurgeBot;
 
 function SurgeBot(){
 	var bot = this;
 
-	// register auth plugin...
+	// register plugins
 	this.auth = auth.auth;
+	this.roll = roll.roll;
 
 	//handle any sort of exit
 	process.on("exit", function(code){
@@ -308,83 +309,6 @@ SurgeBot.prototype.routeHelp = function(requestedArticle){
     }
     //if we make it here, we haven't found a document! Return the error message, replacing {0} with the request
     return Docs.Error(requestedArticle);
-};
-
-//!roll 
-SurgeBot.prototype.roll = function(bot, from, to, params){
-	var argArray = params.split(" "),
-		target = to;
-
-	var args = {
-		die: parseInt(argArray[0]? argArray[0] : ""),
-		min: parseInt(argArray[0]? argArray[0] : ""),
-		max: parseInt(argArray[1]? argArray[1] : ""),
-		mult: (function (multArgs){
-			for( var i=1; i<multArgs.length; i++ ){
-				if( multArgs[i] == "-m" && multArgs[i+1] ){
-					return parseInt(multArgs[i+1]);
-				}
-				else if( multArgs[i] == "-m" ){
-					return NaN;
-				}
-			}
-			return undefined;
-		})(argArray)
-	};
-
-	switch(argArray.length){
-		case 4:
-			//assume !roll min max -m mult
-			if( isNaN(args.min) || isNaN(args.max) || isNaN(args.mult) ){
-				bot.client.say(to, 'Malformed !roll. '
-					+'Expected !roll <min> <max> -m <multiplier>, '
-					+'got !roll <'+argArray[0]+'> <'+argArray[1]+'> <'+argArray[2]+'> <'+argArray[3]+'>. See \'!help roll\' for more information.');
-			}
-			else{
-				bot.client.say(to, from+" rolls a\x0306 "+(Math.floor(Math.random() * (args.max - args.min)) + args.min)*args.mult);
-			}
-		break;
-
-		case 3:
-			//assume !roll die -m mult
-			if( isNaN(args.die) || isNaN(args.mult) ){
-				bot.client.say(to, 'Malformed !roll. '
-					+'Expected !roll <die> -m <multiplier>, '
-					+'got !roll <'+argArray[0]+'> <'+argArray[1]+'> <'+argArray[2]+'>. See \'!help roll\' for more information.');
-			}
-			else{
-				bot.client.say(to, from+" rolls a\x0306 "+(Math.floor(Math.random() * (args.die - 1)) + 1)*args.mult);
-			}
-		break;
-	
-		case 2:
-			//assume !roll min max
-			if( isNaN(args.min) || isNaN(args.max) ){
-				bot.client.say(to, 'Malformed !roll. '
-					+'Expected !roll <min> <max>, '
-					+'got !roll <'+argArray[0]+'> <'+argArray[1]+'>. See \'!help roll\' for more information.');
-			}
-			else{
-				bot.client.say(to, from+" rolls a\x0306 "+(Math.floor(Math.random() * (args.max - args.min)) + args.min) );
-			}
-		break;
-
-		case 1:
-			//assume !roll die
-			if( isNaN(args.die) ){
-				bot.client.say(to, 'Malformed !roll. '
-					+'Expected !roll <die>, '
-					+'got !roll <'+argArray[0]+'>. See \'!help roll\' for more information.');
-			}
-			else{
-				bot.client.say(to, from+" rolls a\x0306 "+(Math.floor(Math.random() * (args.die - 1)) + 1) );
-			}
-		break;
-		
-		default:
-			bot.client.say(to, 'Malformed !roll. See \'!help roll\' for more information.');
-		break;
-	}
 };
 
 //!diceGame <start|end|play> [-b <bid>]
